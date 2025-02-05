03e1a29cfda64dabcff1f8821014f54f3d36b22d
Dhakadshiv <145250778+Dhakadshiv@users.noreply.github.com>
2025-02-05 13:44:40 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,18 @@
-import { Router } from "express";
-import wrapAsync from "../utils/wrapAsnyc.js";
-import ExpressError from "../utils/ExpressError.js";
-import Review from "../models/review.js";
-import Listing from "../models/listing.js";
-import { validateReview, isLoggedIn, isReviewAuther } from "../middleware.js";
-import { createReview, destroyReview } from "../controllers/reviews.js";
-
-
-const router=Router({mergeParams:true});
-
+// (() => {
+//     'use strict'
   
-
-// Reviews
-//post rout
-
-router.post("/", isLoggedIn,validateReview, wrapAsync(createReview ));
-
-
-// Delete review rout
-router.delete("/:reviewId",isLoggedIn ,isReviewAuther,wrapAsync(  destroyReview))
-
-export default router;
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     const forms = document.querySelectorAll('.needs-validation')
+  
+//     // Loop over them and prevent submission
+//     Array.from(forms).forEach(form => {
+//       form.addEventListener('submit', event => {
+//         if (!form.checkValidity()) {
+//           event.preventDefault()
+//           event.stopPropagation()
+//         }
+  
+//         form.classList.add('was-validated')
+//       }, false)
+//     })
+//   })()
